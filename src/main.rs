1016ae6e7b7899caad57b428c5a63d4bfb6dcdea
NT-marlowe <n.tezuka196.ninj@gmail.com>
2022-02-11 21:23:38 +0900

    it takse commandline args and panic

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,19 @@
+use std::env;
 use std::fs;
 
 fn main() {
     println!("mkdir and cd");
-    match fs::create_dir("a") {
+    
+    let argv: Vec<String> = env::args().collect();
+    assert!(argv.len() >= 2, "mcdir: missing operand");
+    // println!("len = {}", argv.len());
+
+    let dir_name = &argv[1];
+    match fs::create_dir(dir_name) {
         Err(why) => println!("! {:?}", why.kind()),
         Ok(_) => {},
     }   
     
-
-    
+    println!("commandline args = {:?}" , argv);
 }
 
